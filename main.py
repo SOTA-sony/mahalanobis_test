8a0d9c4b495532d77014a9684000a8abced5837c
SOTA-sony <153707840+SOTA-sony@users.noreply.github.com>
2023-12-15 16:55:41 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -92,7 +92,6 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
     st.write('教師データと評価データの結合後のデータフレーム:')
     st.write(df)
 
-    # 散布図
     scatter_data = px.scatter(
         df_time, x='DATA_DATETIME', y='mahalanobis_distance',
         color=df_time['mark'],  # 'mark' 列に基づいて色分け
@@ -116,6 +115,71 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
     plt.xticks(np.arange(1, 8, 1))
     plt.grid(True)
     st.pyplot(fig_variance_ratio)
+
+##############################################################################################################################
+    # n = 7 散布図
+        # Streamlitアプリケーション
+    st.title('散布図（第１主成分～第７主成分）')
+
+    df_pca_2d = pd.DataFrame(df_pca, columns=[f"第{i}主成分" for i in range(1,8)])
+    df_2d = pd.concat([df[["EES_WAFER_ID","mark","mahalanobis_distance"]],df_pca_2d],axis=1)
+
+    # 散布図
+    scatter_data = px.scatter(
+        df_2d, x="第1主成分", y="第2主成分",
+        color="mahalanobis_distance",  # マハラノビス距離をカラーに設定
+        color_continuous_scale=['blue', 'red'],
+        symbol="mark",  # マーカーの形を "symbol_marker" 列に基づいて設定
+        symbol_sequence=['circle', 'x'],
+        labels={'第1主成分': '第1主成分軸', '第2主成分': '第2主成分軸', 'EES_WAFER_ID': 'ウェーハID', 'mahalanobis_distance': 'マハラノビス距離'},
+        title='主成分空間上の散布図',
+        hover_data={'EES_WAFER_ID': True}  # カーソルを当てた際に表示するデータを指定
+    )
+
+    # カラーバーの位置を下げる
+    scatter_data.update_layout(coloraxis_colorbar=dict(yanchor="top", y=5, ypad=20))
+
+    # 原点を中心にするための範囲設定
+    max_range = max(abs(df_2d["第1主成分"]).max(), abs(df_2d["第2主成分"]).max())
+    scatter_data.update_layout(
+        xaxis=dict(range=[-max_range, max_range]),
+        yaxis=dict(range=[-max_range, max_range]),
+    )
+
+
+    # 原点を交差する垂直な直線
+    scatter_data.add_shape(
+        type='line',
+        x0=0, x1=0,
+        y0=-max_range, y1=max_range,
+        line=dict(color='black', width=1)
+    )
+
+    # 原点を交差する水平な直線
+    scatter_data.add_shape(
+        type='line',
+        x0=-max_range, x1=max_range,
+        y0=0, y1=0,
+        line=dict(color='black', width=1)
+    )
+
+    # 散布図を表示
+    st.plotly_chart(scatter_data)
+
+    # クリックされた点の情報
+    selected_points_info = []
+
+    # チェックボックスを使用して複数選択
+    selected_wafer_ids = st.multiselect("ウェーハIDを選択してテーブルを表示", df_2d["EES_WAFER_ID"])
+
+    # 選択された点の情報を取得
+    for wafer_id in selected_wafer_ids:
+        selected_points_info.append(df[df["EES_WAFER_ID"] == wafer_id])
+
+    # 選択された点の情報をテーブルで表示
+    if selected_points_info:
+        selected_point_df = pd.concat(selected_points_info)
+        st.table(selected_point_df)
 ##############################################################################################################################
     # 主成分分析（PCA）を実行
     pca = PCA(n_components=2)  
@@ -132,7 +196,7 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
     df['mahalanobis_distance_2'] = df['mahalanobis_distance_2'].apply(lambda x: round(x, 3))
  
     # Streamlitアプリケーション
-    st.title('散布図')
+    st.title('散布図（第１主成分～第２主成分）')
 
     df_pca_2d = pd.DataFrame(df_pca, columns=[f"第{i}主成分" for i in range(1,3)])
     df_2d = pd.concat([df[["EES_WAFER_ID","mark","mahalanobis_distance_2"]],df_pca_2d],axis=1)
