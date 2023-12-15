50d04e8bace9b70aa06c757d9b3c8fe240f89b21
SOTA-sony <153707840+SOTA-sony@users.noreply.github.com>
2023-12-15 16:43:37 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sklearn.decomposition import PCA
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 import plotly.express as px
-
 # アプリケーションの背景色を設定
 st.set_page_config(
     page_title="PCA & Mahalanobis Distance Tool",
@@ -26,8 +25,10 @@ uploaded_test_file = st.sidebar.file_uploader("評価データを選択してく
 if uploaded_train_file is not None and uploaded_test_file is not None:
     # 教師データ読み込み
     df_train = pd.read_excel(uploaded_train_file)
+    df_train["mark"] = "教師データ"
     # 評価データ読み込み
     df_test = pd.read_excel(uploaded_test_file)
+    df_test["mark"] = "評価データ"
     # 教師データと評価データの結合
     df = pd.concat([df_train, df_test]).reset_index(drop=True)
 
@@ -76,7 +77,7 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
    # "DATA_DATETIME" を含む列名を除外して float_columns を作成
     float_columns = df.select_dtypes(include='float').columns.tolist()
     # 日時列と float 列を分ける
-    df_time = df[["DATA_DATETIME"] + float_columns].copy()
+    df_time = df[["DATA_DATETIME"] + ["mark"] + float_columns].copy()
 
     # 日時列を datetime フォーマットに変換
     df_time["DATA_DATETIME"] = pd.to_datetime(df_time["DATA_DATETIME"], format='%Y/%m/%d %H:%M:%S')
@@ -91,9 +92,18 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
     st.write('教師データと評価データの結合後のデータフレーム:')
     st.write(df)
 
-    # マハラノビス距離のプロットを散布図に変更
-    st.write('マハラノビス距離のプロット:')
-    st.scatter_chart(df_time.set_index('DATA_DATETIME')[['mahalanobis_distance']])
+    # 散布図
+    scatter_data = px.scatter(
+        df_time, x='DATA_DATETIME', y='mahalanobis_distance',
+        color=df_time['mark'],  # 'mark' 列に基づいて色分け
+        color_discrete_map={'教師データ': 'blue', '評価データ': 'red'},
+        labels={'mahalanobis_distance': 'マハラノビス距離'},
+        title='マハラノビス距離のプロット',
+        hover_data={'DATA_DATETIME': True, 'mahalanobis_distance': True, 'mark': True}  # カーソルを当てた際に表示するデータを指定
+    )
+
+    # 散布図を表示
+    st.plotly_chart(scatter_data)
 
 
     # 主成分数ごとの累積寄与率の折れ線グラフ
@@ -106,29 +116,49 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
     plt.xticks(np.arange(1, 8, 1))
     plt.grid(True)
     st.pyplot(fig_variance_ratio)
+##############################################################################################################################
+    # 主成分分析（PCA）を実行
+    pca = PCA(n_components=2)  
+    df_pca = pca.fit_transform(df.loc[:,float_columns])
 
+    # マハラノビス距離の計算
+    mean_vector = df_pca.mean(axis=0)
+    covariance_matrix = np.cov(df_pca, rowvar=False)
+    covariance_matrix_inv = np.linalg.inv(covariance_matrix)
+
+    # Mahalanobis distanceを計算して新しい列に追加
+    df['mahalanobis_distance_2'] = [distance.mahalanobis(x, mean_vector, covariance_matrix_inv) for x in df_pca]
+    # マハラノビス距離を小数点3桁まで表示
+    df['mahalanobis_distance_2'] = df['mahalanobis_distance_2'].apply(lambda x: round(x, 3))
+ 
     # Streamlitアプリケーション
-    st.title('2D散布図')
+    st.title('散布図')
 
-    df_pca_2d = pd.DataFrame(df_pca, columns=["1","2","3","4","5","6","7"])
-    df_2d = pd.concat([df[["EES_WAFER_ID","mahalanobis_distance"]],df_pca_2d],axis=1)
+    df_pca_2d = pd.DataFrame(df_pca, columns=[f"第{i}主成分" for i in range(1,3)])
+    df_2d = pd.concat([df[["EES_WAFER_ID","mark","mahalanobis_distance_2"]],df_pca_2d],axis=1)
 
     # 散布図
     scatter_data = px.scatter(
-        df_2d, x="1", y="2",
-        color="mahalanobis_distance",  # マハラノビス距離をカラーに設定
+        df_2d, x="第1主成分", y="第2主成分",
+        color="mahalanobis_distance_2",  # マハラノビス距離をカラーに設定
         color_continuous_scale=['blue', 'red'],
-        labels={'1': '第1主成分軸', '2': '第2主成分軸', 'EES_WAFER_ID': 'ウェーハID', 'mahalanobis_distance': 'マハラノビス距離'},
+        symbol="mark",  # マーカーの形を "symbol_marker" 列に基づいて設定
+        symbol_sequence=['circle', 'x'],
+        labels={'第1主成分': '第1主成分軸', '第2主成分': '第2主成分軸', 'EES_WAFER_ID': 'ウェーハID', 'mahalanobis_distance_2': 'マハラノビス距離'},
         title='主成分空間上の散布図',
         hover_data={'EES_WAFER_ID': True}  # カーソルを当てた際に表示するデータを指定
     )
 
+    # カラーバーの位置を下げる
+    scatter_data.update_layout(coloraxis_colorbar=dict(yanchor="top", y=5, ypad=20))
+
     # 原点を中心にするための範囲設定
-    max_range = max(abs(df_2d["1"]).max(), abs(df_2d["2"]).max())
+    max_range = max(abs(df_2d["第1主成分"]).max(), abs(df_2d["第2主成分"]).max())
     scatter_data.update_layout(
         xaxis=dict(range=[-max_range, max_range]),
         yaxis=dict(range=[-max_range, max_range]),
     )
+
 
     # 原点を交差する垂直な直線
     scatter_data.add_shape(
@@ -165,6 +195,20 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
         st.table(selected_point_df)
 
 #############################################################################################
+    # 主成分分析（PCA）を実行
+    pca = PCA(n_components=3)  # 主成分数を7までに制限
+    df_pca = pca.fit_transform(df.loc[:,float_columns])
+
+    # マハラノビス距離の計算
+    mean_vector = df_pca.mean(axis=0)
+    covariance_matrix = np.cov(df_pca, rowvar=False)
+    covariance_matrix_inv = np.linalg.inv(covariance_matrix)
+
+    # Mahalanobis distanceを計算して新しい列に追加
+    df['mahalanobis_distance_3'] = [distance.mahalanobis(x, mean_vector, covariance_matrix_inv) for x in df_pca]
+    # マハラノビス距離を小数点3桁まで表示
+    df['mahalanobis_distance_3'] = df['mahalanobis_distance_3'].apply(lambda x: round(x, 3))
+    
     # 主成分空間での3D散布図
     st.title('3D散布図')
     fig_3d = px.scatter_3d(
@@ -172,12 +216,17 @@ if uploaded_train_file is not None and uploaded_test_file is not None:
         x=df_pca[:, 0],
         y=df_pca[:, 1],
         z=df_pca[:, 2],  # 第三主成分をZ軸に追加
-        color=df['mahalanobis_distance'],  # マハラノビス距離を色で表現
+        color=df['mahalanobis_distance_3'],  # マハラノビス距離を色で表現
+        symbol="mark",
+        symbol_sequence=['circle', 'x'],
         size_max=50,
         labels={'x': '第１主成分軸', 'y': '第２主成分軸', 'z': '第３主成分軸','color': 'マハラノビス距離'},
         title="主成分空間内の3D散布図",
         color_continuous_scale=['blue', 'red'], # 'Turbo'
     )
+
+    # カラーバーの位置を下げる
+    fig_3d.update_layout(coloraxis_colorbar=dict(yanchor="top", y=5, ypad=20))
 
     # 3D Scatter Plotを表示
     st.plotly_chart(fig_3d)
